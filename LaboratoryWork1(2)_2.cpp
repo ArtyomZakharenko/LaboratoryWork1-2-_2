4391691d4ecbc3fbd6aa40db0df5fb443b9e52e6
@artiomzaharenko <artiomzaharenko@gmail.com>
2021-03-08 23:32:39 +0300

    update

--- LaboratoryWork1(2)_2.cpp
+++ LaboratoryWork1(2)_2.cpp
@@ -17,9 +17,9 @@ struct film * CreateStructArray(int);
 void FillStructArray(struct film*, int);
 
 void ShowOptions();
-void OperationsWithArray();
+void OperationsWithArray(struct film*, int);
 void FindFilm (struct film *, int);
-
+void DeleteFilm(struct film *, int);
 
 int main()
 {
@@ -45,7 +45,6 @@ struct film* CreateStructArray(int size) {
 void FillStructArray(struct film* arr, int size) {
 	int i;
 
-	
 	for (i = 0; i < size; i++) {
 		getchar();
 		arr[i].name = (char*)calloc(20, sizeof(char));
@@ -73,7 +72,6 @@ void ShowStructArray(struct film* arr, int size) {
 }
 
 void ShowOptions() {
-	int option;
 	printf("What would you like to do with the library?\n");
 	printf("1. Find film with the same name\n");
 	printf("2. Delete film with length longer than entered\n");
@@ -87,6 +85,7 @@ void OperationsWithArray(struct film* arr, int size) {
     
 	switch (opt) {
 	case 1: 
+		FindFilm(arr, size);
 		break;
 	case 2:
 		break;
@@ -105,13 +104,68 @@ void OperationsWithArray(struct film* arr, int size) {
 void FindFilm (struct film * arr, int size){
     char key[20];
     int i;
+	bool isExist = false;
     printf ("Enter film name you want to find: ");
     scanf("%19s", &key);
     for (i = 0; i < size; i++){
-        if (strcmp(arr[i].film.name, key) == 0){
-            printf ()
+        if (strcmp(arr[i].name, key) == 0){
+			isExist = true;
+			printf("We found film with the same name:");
+			printf("Film %d\n", i + 1);
+			printf("\nName: %s", arr[i].name);
+			printf("Length: %d\n", arr[i].length);
+			printf("Genre: %s\n", arr[i].info.genre);
+			printf("Year: %d", arr[i].info.year);
+			printf("\n");
         }
     }
+	if (!isExist) {
+		printf("We found nothing with the same name :(");
+	}
     
-    
+}
+
+void DeleteFilm(struct film* arr, int size) {
+	int key;
+	int amount;
+
+	printf("Enter length film: ");
+	scanf("%d", &key);
+
+	amount = CountElements(arr, size, key);
+	if (amount != 0) {
+		ReplaceElemByKey(arr, size, key);
+
+	}
+	
+}
+
+void ReplaceElemByKey(struct film* arr, int size, int key) {
+	int i, j, temp;
+	
+
+	for (i = 0; i < size; i++) {
+		for (j = size - 1; j > i; j--) {
+			if ((arr[j - 1].length > key) && (arr[j].length) <= key) {
+				temp = arr[j - 1].length;
+				arr[j - 1].length = arr[j].length;
+				arr[j].length = temp;
+			}
+		}
+	}
+}
+
+int CountElements(struct film* arr, int size, int key) {
+	int i, counter = 0;
+	for (i = 0; i < size; i++) {
+		if (key < arr[i].length) {
+			counter++;
+		}
+	}
+
+	return counter;
+}
+
+void ReallocateArray(struct film* arr, int size, int count) {
+	arr = (struct film*)realloc(arr, (size-count)*sizeof(struct film));
 }
